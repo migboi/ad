531099a3f712eee1f7a652639bc4c0a99e275d41
migboi <migboi@alu.iesserpis.org>
2019-09-20 09:34:13 +0200

    cHolaMundo/

---
+++ cHolaMundo/cHolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cHolaMundo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
